7baed6607ae5495de3ca673d5a3be1692335ceb9
PaolaJuliette <paolajuliette2002@gmail.com>
2025-07-27 00:06:20 -0400

    instalacion de react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Paginá en construcción</h1>
+      <p>por favor, no quitar el icono de la pestaña estoy esperando que ahi pondre el logo gracias, besos </p>
+    </div>
+  )
+}
+
+export default App
