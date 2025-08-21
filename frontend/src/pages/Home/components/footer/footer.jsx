ec107da580fc24426a7db203561e95320398ab7f
GarWox <gchin84@gmail.com>
2025-08-21 16:10:02 -0400

    Añadiendo Responsive a Footer

--- frontend/src/pages/Home/components/footer/footer.jsx
+++ frontend/src/pages/Home/components/footer/footer.jsx
@@ -7,89 +7,98 @@ import "../../../../assets/styles/footer.css";
 const footer = () => {
   return (
     <footer>
-      <div className="info-container">
-        <div>
-          <img src={logo} alt="Adiemi" />
-        </div>
-        {/* PRODUCTOS */}
-        <section className="productos-footer">
-          <h3>Productos</h3>
-          <ul>
-            <li>
-              <a href="#">Promociones</a>
-            </li>
-            <li>
-              <a href="#">¿Cómo hacer tu pedido?</a>
-            </li>
-            <li>
-              <a href="#">Blog</a>
-            </li>
-          </ul>
-        </section>
-        {/* SOBRE NOSOTROS */}
-        <section className="about-us">
-          <h3>Sobre Nosotros</h3>
-          <ul>
-            <li>
-              <a href="#">¿Quiénes somos?</a>
-            </li>
-            <li>
-              <a href="#">Ubicación</a>
-            </li>
-            <li>
-              <a href="#">Gerentes</a>
-            </li>
-          </ul>
-        </section>
-        {/* FAQ */}
-        <section className="faq">
-          <h3>Ayuda</h3>
-          <ul>
-            <li>
-              <a href="#">Preguntas Frecuentes</a>
-            </li>
-            <li>
-              <a href="#">Términos y condiciones</a>
-            </li>
-            <li>
-              <a href="#">Contacto</a>
-            </li>
-          </ul>
-        </section>
+      <div className="footer-logo-container">
+        <img className="footer-logo" src={logo} alt="Adiemi" />
       </div>
+      {/* PRODUCTOS */}
+      <section className="productos-footer">
+        <h3>Productos</h3>
+        <ul>
+          <li>
+            <a href="#">¿Cómo hacer tu pedido?</a>
+          </li>
+          <li>
+            <a href="#">Promociones</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+        </ul>
+      </section>
+      {/* SOBRE NOSOTROS */}
+      <section className="about-us">
+        <h3>Sobre Nosotros</h3>
+        <ul>
+          <li>
+            <a href="#">¿Quiénes somos?</a>
+          </li>
+          <li>
+            <a href="#">Ubicación</a>
+          </li>
+          <li>
+            <a href="#">Gerentes</a>
+          </li>
+        </ul>
+      </section>
+      {/* FAQ */}
+      <section className="faq">
+        <h3>Ayuda</h3>
+        <ul>
+          <li>
+            <a href="#">Preguntas Frecuentes</a>
+          </li>
+          <li>
+            <a href="#">Términos y condiciones</a>
+          </li>
+          <li>
+            <a href="#">Contacto</a>
+          </li>
+        </ul>
+      </section>
       <br />
       {/*              */}
       {/* SOCIAL MEDIA */}
       {/*              */}
-      <div className="social-media-container">
+      <section className="social-media-section">
         <h3>¡Siguenos en nuestras redes sociales!</h3>
-        <section>
-          <a
-            href="https://www.instagram.com/promocionesadiemi/"
-            target="_blank"
-          >
-            <FaInstagram />
-          </a>
-          <a
-            href="https://www.facebook.com/adiemi.adiemi.1?locale=es_LA"
-            target="_blank"
-          >
-            <FaFacebook />
-          </a>
-          <a
-            href="https://www.tiktok.com/@promosadiemioficial?_t=ZM-8yzDBPngdqa&_r=1"
-            target="_blank"
-          >
-            <AiFillTikTok />
-          </a>
-          <a href="https://www.youtube.com/@promocionesAdiemi" target="_blank">
-            <FaYoutube />
-          </a>
-        </section>
-      </div>
+        <ul>
+          <li>
+            <a
+              href="https://www.instagram.com/promocionesadiemi/"
+              target="_blank"
+            >
+              <FaInstagram />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/adiemi.adiemi.1?locale=es_LA"
+              target="_blank"
+            >
+              <FaFacebook />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.tiktok.com/@promosadiemioficial?_t=ZM-8yzDBPngdqa&_r=1"
+              target="_blank"
+            >
+              <AiFillTikTok />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/@promocionesAdiemi"
+              target="_blank"
+            >
+              <FaYoutube />
+            </a>
+          </li>
+        </ul>
+      </section>
       {/* COPYRIGHT */}
       <br />
-      <div className="footer-copyright">
+      <div className="copyright">
         <p>
           &copy; {new Date().getFullYear()} Promociones Adiemi. Todos los
           derechos reservados.
