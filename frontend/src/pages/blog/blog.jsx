284f0fb05aff55d16581debb72b7fd8f6f50ca9a
GarWox <gchin84@gmail.com>
2025-08-22 01:55:33 -0400

    Blog sitio en construccion editado

--- frontend/src/pages/blog/blog.jsx
+++ frontend/src/pages/blog/blog.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import Logo from '../../assets/images/logoAdiemi.png'
+import React from "react";
+import Logo from "../../assets/images/logoAdiemi.png";
+import "../../assets/styles/blog.css";
 
 const Blog = () => {
   return (
-    <div>
-      <div>
+    <section className="construction-section">
+      <div className="logo-container">
         <img src={Logo} alt="Adiemi" />
       </div>
-      <div>
-        <h3>Lo siento, estamos en</h3>
-        <h1>CONSTRUCCIÓN</h1>
-        <p>Nos vemos pronto en la página oficial de <span>Promociones Adiemi</span></p>
+      <div className="construction-message">
+        <h1>
+          Lo siento, estamos en <span>CONSTRUCCIÓN</span>
+        </h1>
+        <p>
+          Nos vemos pronto en la página oficial de{" "}
+          <span>Promociones Adiemi</span>
+        </p>
       </div>
-    </div>
-  )
-}
+    </section>
+  );
+};
 
-export default Blog
+export default Blog;
